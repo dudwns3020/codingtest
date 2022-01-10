8dc55a2ed6951c3e31bf7f03bda7deee663a0b69
dudwns3020 <68069311+dudwns3020@users.noreply.github.com>
2022-01-10 23:42:36 +0900

    upload new_id_recom

--- new_id_recom.java
+++ new_id_recom.java
@@ -1,11 +1,43 @@
 class Solution {
     public String solution(String new_id) {
         String answer = "";
-    
+        //1
         answer = new_id.toLowerCase();
         
-        String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z\\s-_.]";
-        answer = answer.replaceAll(match, "");
+        //2
+        answer = answer.replaceAll("[^a-z0-9-_.]", "");
+        
+        //3
+        //for(int i = 0 ; i < answer.length() - 1 ; i++ ){
+        //    if(answer.charAt(i) == answer.charAt(i + 1)) {
+        //        answer = answer.replace(answer.charAt(i), "");
+        //    }
+        //}
+        
+        //4
+        if(answer.charAt(0) == '.') {
+            answer = answer.substring(1, answer.length());
+        }
+        //else if(answer.charAt(answer.length()) == '.') {
+        //    answer = answer.substring(0, answer.length()-1);
+        //}
+
+        //5
+        if(answer.length() == 0) {
+            answer = "a";
+        }
+
+        //6
+        if(answer.length() >= 16) {
+            answer.substring(0, 15);
+        }
+        
+        //7
+         if(answer.length() <= 2) {
+            while(answer.length() < 3){
+                answer += answer.charAt(0);
+            }        
+        }
         
         return answer;
     }

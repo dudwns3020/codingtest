a1ce0213bc27f4b9cc37a4a773a5d9e7a05e7ece
dudwns3020 <dudwns3020@naver.com>
2022-04-20 23:42:50 +0900

    add add_missing_numbers

---
+++ add_missing_numbers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int len = 9;
+        
+        for(int i = 0; i <= len; i++) {
+            answer += i;
+        }
+        
+        for(int i = 0; i < numbers.length; i++) {
+            answer -= numbers[i];
+        }
+        
+        return answer;
+    }
+}

ed6acaa76258e66947996e22261f5e0a84e1b8b5
dudwns3020 <dudwns3020@naver.com>
2022-04-23 22:20:44 +0900

    upload numeric_string_and_english_word.java

--- numeric_string_and_english_word.java
+++ numeric_string_and_english_word.java
@@ -2,18 +2,15 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         
-        String a = s.replace("one", "1");
-        a = a.replace("two", "2");
-        a = a.replace("three", "3");
-        a = a.replace("four", "4");
-        a = a.replace("five", "5");
-        a = a.replace("six", "6");
-        a = a.replace("seven", "7");
-        a = a.replace("eight", "8");
-        a = a.replace("nine", "9");
+        String[] str = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
         
-        answer = Integer.parseInt(a);
-        
+        for(int i = 0; i < str.length; i++) {
+            s = s.replace(str[i], num[i]);
+        }
+
+        answer = Integer.parseInt(s);
+
         return answer;
     }
 }

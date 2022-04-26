0f85ee91a2e18a053a5fb6a08a76816c82330650
dudwns3020 <dudwns3020@naver.com>
2022-04-26 23:32:02 +0900

    upload push_keypad.java

--- push_keypad.java
+++ push_keypad.java
@@ -1,12 +1,10 @@
 class Solution {
-    int l = 0;
-    int r = 0;
-    
     public String solution(int[] numbers, String hand) {
         String answer = "";
+        int l = 0;
+        int r = 0;
         
-        for(int i = 0; i < numbers.length; i++) {
-            
+        for(int i = 0; i < numbers.length; i++) {            
             if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7) {
                 l = numbers[i];
                 answer += "L";
@@ -16,27 +14,14 @@ class Solution {
                 answer += "R";
             }
             else {
-                answer += len(numbers[i], hand);
+                answer += len(numbers[i], hand, r, l);
             }
         }
         
         return answer;
     }
     
-    public String len (int numbers, String hand) {
-        if(Math.abs(numbers - r) > Math.abs(numbers - l)) {
-            return "L";
-        }
-        else if(Math.abs(numbers - r) == Math.abs(numbers - l)) {
-            if (Math.abs(numbers - r) > Math.abs(numbers - l)) {
-                return "L";
-            }
-            else {
-                return "R";
-            }
-        }
-        else {
-            return "R";
-        }
+    public String len (int numbers, String hand, int r, int l) {
+        
     }
 }

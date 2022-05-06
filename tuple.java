973d7abf5dc560adf70ec7c44f6602ed80ae2462
dudwns3020 <dudwns3020@naver.com>
2022-05-06 18:58:26 +0900

    upload tuple.java

--- tuple.java
+++ tuple.java
@@ -1,21 +1,31 @@
 import java.util.*;
+import java.util.Map.*;
 
 class Solution {
     public int[] solution(String s) {
         int[] answer = {};
-        String str = s.replaceAll("[{,},,]","");
-        
-        int[] num = new int[str.length()];
-        for(int i = 0; i < str.length(); i++) {
-            num [i] = Integer.parseInt(str.substring(i, i+1));
-        }
+        String str = s.replaceAll("[{}]","");
+        String[] sp = str.split(",");
 
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        HashMap<String, Integer> hm = new HashMap<>();
         
-        for(int i = 0; i < str.length(); i++) {
-            hm.put(num[i], i);
+        for(String a : sp) {
+            hm.put(a, hm.getOrDefault(a, 0) + 1);
         }
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(hm.entrySet());
 
+		Collections.sort(list, new Comparator<>() {
+            public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2) {
+                return obj2.getValue().compareTo(obj1.getValue());
+			}
+		});
+
+        answer = new int[list.size()];
+
+        for(int i = 0; i < answer.length; i ++) {
+            answer[i] = Integer.parseInt(list.get(i).getKey());
+        }
+       
         return answer;
     }
 }

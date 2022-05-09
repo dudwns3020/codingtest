b9cbffaa531a8215197962c49e85eee395af7f94
dudwns3020 <dudwns3020@naver.com>
2022-05-09 21:37:35 +0900

    parenthesis_conversion.java

--- parenthesis_conversion.java
+++ parenthesis_conversion.java
@@ -1,25 +1,53 @@
 class Solution {
     public String solution(String p) {
         String answer = "";
+        String u = "";
+        String v = "";
+        if(p.isEmpty()) {
+            return p;
+        }
+        
+        int leftp = 0;
+        int rightp = 0;
         
         for(int i = 0; i < p.length(); i++) {
-            int leftp = 0;
-            int rightp = 0;
-            
             String parenthesis = p.substring(i, i+1);
-            if(parenthesis.equals('{')) {
+            if(parenthesis.equals("(")) {
                 leftp++;
             }
-            else if(parenthesis.equals('}')) {
+            else if(parenthesis.equals(")")) {
                 rightp++;
             }
             
             if(leftp == rightp) {
-                String u = p.substring(0, i+1);
-                String v = p.substring(i+1, p.length());
-                p = v;
+                u = p.substring(0, i+1);
+                v = p.substring(i+1, p.length());
+                break;
             }
         }
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append("(");
+        sb.append(solution(v));
+        sb.append(")");
+        u = u.substring(1, u.length()-1);
+        sb.append(swap(u));
+        
+        answer = u.toString();
+        
         return answer;
+    }
+    
+    public String swap(String str) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str.length(); i++) {
+            if(str.substring(i, i+1).equals("(")) {
+                sb.append(")");
+            }
+            else if(str.substring(i, i+1).equals(")")) {
+                sb.append("(");
+            }
+        }
+        return sb.toString();
     }
 }

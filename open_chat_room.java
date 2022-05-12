2592294d72eeb39155920512a918f0065d0a9709
dudwns3020 <dudwns3020@naver.com>
2022-05-12 15:56:58 +0900

    upload open_chat_room.java

--- open_chat_room.java
+++ open_chat_room.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
-        String[] answer = {};
+        String[] answer = {};        
         
         HashMap<String, String> hm = new HashMap<>();
         
@@ -12,13 +12,27 @@ class Solution {
                 hm.put(str[1], str[2]);
             }
             else if(str[0].equals("Leave")) {
-                hm.remove(str[1]);
+                //hm.remove(str[1]);
             }
             else if(str[0].equals("Change")) {
-                hm.put(str[1], str[2]);
+                hm.put(str[1], str[2]);                
             }
         }
-                
+        
+        List<String> list = new ArrayList<>();
+
+        for(int i = 0; i < record.length; i++) {
+            String[] str = record[i].split(" ");
+            if(str[0].equals("Enter")) {
+                list.add(hm.get(str[1]) + "님이 들어왔습니다.");
+            }
+            else if(str[0].equals("Leave")) {
+                list.add(hm.get(str[1]) + "님이 나갔습니다.");
+            }
+        }
+        
+        answer = list.toArray(answer);
+        
         return answer;
     }
 }

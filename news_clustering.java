a5711ba5eff754369313370dbe6c3267d337ee13
dudwns3020 <dudwns3020@naver.com>
2022-05-18 23:18:59 +0900

    upload news_clustering.java

--- news_clustering.java
+++ news_clustering.java
@@ -19,61 +19,18 @@ class Solution {
                 al2.add(str2.substring(i, i + 2).toUpperCase());
             }
         }
-        System.out.println("");
-         System.out.print("al1 : ");        
-        for(String s: al1) {
-            System.out.print(s + " ");
-        }
-        System.out.println("");
-       System.out.print("al2 : ");        
-        for(String s: al2) {
-            System.out.print(s + " ");
-        }
-        System.out.println("");
-        ArrayList<String> intersection = new ArrayList<>();
-        
-        for(int i = 0; i < al1.size(); i++) {
-            for(int j = 0; j < al2.size(); j++) {
-                if(al1.get(i).equals(al2.get(j))) {
-                    intersection.add(al1.get(i));
-                    // System.out.println(al1.get(i));
-                    // System.out.println(al2.get(j));
-                    al1.remove(i);
-                    al2.remove(j);
-                    i = 0;
-                    j = 0;
-                }
-            }
-        }        
-        
-        System.out.print("N : ");
-        for(String s: intersection) {
-            System.out.print(s + " ");
-        }
-        System.out.println("");
-         System.out.print("al1 : ");        
-        for(String s: al1) {
-            System.out.print(s + " ");
-        }
-        System.out.println("");
-       System.out.print("al2 : ");        
-        for(String s: al2) {
-            System.out.print(s + " ");
-        }
-        System.out.println("");
-        
-        ArrayList<String> union = new ArrayList<>();
-        for(String s: al1) {
-            union.add(s);
-        }
-        for(String s: al2) {
-            union.add(s);
-        }
-        for(String s: intersection) {
-            union.add(s);
-        }
 
+        ArrayList<String> intersection = new ArrayList<>();
+        ArrayList<String> union = new ArrayList<>();
+        
+        for(String s: al1) {
+            if(al2.remove(s)) {
+                intersection.add(s);
+            }
+        }
+        
         double similar = (double) intersection.size()/union.size();
+        
         if(similar == 0) {
             similar = 1;
         }

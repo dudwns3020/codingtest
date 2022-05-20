3965163758e6130a081104e87a3dd21596ae3abf
dudwns3020 <dudwns3020@naver.com>
2022-05-20 23:57:29 +0900

    add menu_renewal.java

---
+++ menu_renewal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+        
+        for(String s: orders) {
+            for(int a: course) {
+                
+            }
+        }
+        
+        return answer;
+    }
+    
+    public void combidation(int[] arr, boolean[] visited, int n) {
+        
+    }
+}

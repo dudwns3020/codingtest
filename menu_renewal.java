0af8546899c8705a6b538b38bdcd332af7a78b36
dudwns3020 <dudwns3020@naver.com>
2022-05-23 22:14:46 +0900

    upload menu_renweal.java

--- menu_renewal.java
+++ menu_renewal.java
@@ -1,18 +1,38 @@
+import java.util.*;
+
 class Solution {
     public String[] solution(String[] orders, int[] course) {
         String[] answer = {};
+        HashMap<String, Integer> hm = new HashMap<>();
         
-        for(int i = 0; i < orders.length; i++) {
-            char[] orders_char = orders[i].toCharArray();
-            for(int j = 0; j < course.length; j++) {
-                // dfs(orders_char, course[j], 0);
-                }
+        for(String s: orders) {
+            char[] orders_char = s.toCharArray();
+            int len = orders_char.length;
+            boolean[] visited = new boolean[len];
+            for(int i = 0; i < course.length; i++) {
+                combi(orders_char, visited, 0, len, course[i]);
+            }
+            System.out.println();
         }
         
         return answer;
     }
     
-    public void dfs(char[] arr, boolean[] visited, int n) {
+    void combi(char[] arr, boolean[] visited, int s, int n, int r) {
+        String str = "";
+        if(r == 0) {
+            for(int i = 0; i < n; i++) {
+                if(visited[i]) {
+                    str += arr[i];
+                }
+            }
+            System.out.println(str);
+        }
         
+        for(int i = s; i < n; i++) {
+            visited[i] = true;
+            combi(arr, visited, i+1, n, r-1);
+            visited[i] = false;
+        }
     }
 }

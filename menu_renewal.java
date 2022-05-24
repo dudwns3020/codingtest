3b412be4eb561b0a445f654db401418864e1583c
dudwns3020 <dudwns3020@naver.com>
2022-05-24 23:07:04 +0900

    menu_renewal.java

--- menu_renewal.java
+++ menu_renewal.java
@@ -1,18 +1,25 @@
 import java.util.*;
 
 class Solution {
+    
+    HashMap<String, Integer> hm = new HashMap<>();
+
     public String[] solution(String[] orders, int[] course) {
         String[] answer = {};
-        HashMap<String, Integer> hm = new HashMap<>();
-        
+
         for(String s: orders) {
             char[] orders_char = s.toCharArray();
+            Arrays.sort(orders_char);
             int len = orders_char.length;
             boolean[] visited = new boolean[len];
             for(int i = 0; i < course.length; i++) {
                 combi(orders_char, visited, 0, len, course[i]);
             }
-            System.out.println();
+        }
+        for(String a: hm.keySet()) {
+            if(a.length() == 2) {
+            System.out.println(a + ", " +hm.get(a));
+            }
         }
         
         return answer;
@@ -26,7 +33,7 @@ class Solution {
                     str += arr[i];
                 }
             }
-            System.out.println(str);
+            hm.put(str,hm.getOrDefault(str,0) + 1);
         }
         
         for(int i = s; i < n; i++) {

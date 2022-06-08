e1e74db6bf8d62a61e3cc6b28ca7e81582515fe4
dudwns3020 <dudwns3020@naver.com>
2022-06-08 23:50:34 +0900

    add operator_maximization.java

---
+++ operator_maximization.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public long solution(String expression) {
+        long answer = 0;
+        
+        char[] c = new char[expression.length()];
+        
+        for(int i = 0; i < expression.length(); i++) {
+            c[i] = expression.charAt(i);
+        }
+        
+        return answer;
+    }
+}

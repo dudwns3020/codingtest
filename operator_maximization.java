1bb60403e0fd67aec4962d213b560126ecc333b0
dudwns3020 <dudwns3020@naver.com>
2022-06-09 19:36:03 +0900

    upload operator_maximization.java

--- operator_maximization.java
+++ operator_maximization.java
@@ -4,10 +4,22 @@ class Solution {
     public long solution(String expression) {
         long answer = 0;
         
-        char[] c = new char[expression.length()];
+        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<Character> operators = new ArrayList<>();
         
-        for(int i = 0; i < expression.length(); i++) {
-            c[i] = expression.charAt(i);
+        String expressnumber =  expression.replaceAll("[^0-9]", " ");
+        String[] number = expressnumber.split(" ");
+        int[] num = new int[number.length];
+        for(int i = 0; i < number.length; i++) {
+            num[i] = Integer.parseInt(number[i]);
+            numbers.add(num[i]);
+        }
+        
+        String expressoper =  expression.replaceAll("[0-9]", "");
+        for(int i = 0; i < expressoper.length(); i++) {
+            if(!operators.contains(expressoper.charAt(i))) {
+                operators.add(expressoper.charAt(i));
+            }
         }
         
         return answer;

11a963762b074ad45787d641b503f048bfe1d0bd
dudwns3020 <dudwns3020@naver.com>
2022-06-10 23:26:23 +0900

    upload operator_maximization.java

--- operator_maximization.java
+++ operator_maximization.java
@@ -1,11 +1,14 @@
 import java.util.*;
 
 class Solution {
+    ArrayList<Integer> numbers;
+    ArrayList<Character> operators;
+    ArrayList<String> operpermu;
     public long solution(String expression) {
         long answer = 0;
         
-        ArrayList<Integer> numbers = new ArrayList<>();
-        ArrayList<Character> operators = new ArrayList<>();
+        numbers = new ArrayList<>();
+        operators = new ArrayList<>();
         
         String expressnumber =  expression.replaceAll("[^0-9]", " ");
         String[] number = expressnumber.split(" ");
@@ -22,6 +25,34 @@ class Solution {
             }
         }
         
+        int size = 1;
+        for(int i = 1; i <= operators.size(); i++) {
+            size *= i;
+        }
+        
+        boolean[] visited = new boolean[operators.size()];
+        String[] output = new String[size];
+        operpermu = new ArrayList<>();
+        
+        permu(operators, operpermu, output, visited, 0, operators.size());
+        for(String s: operpermu) {
+            System.out.println(s);
+        }
         return answer;
+    }
+    
+    public void permu(ArrayList<Character> operators, ArrayList<String> operpermu, String[] output, boolean[] visited, int depth, int r) {
+        if(depth == r) {
+            operpermu.add(output.toString());
+            return;
+        }
+        for(int i = 0; i < operators.size(); i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                output[depth] = operators.get(i).toString();
+                permu(operators, operpermu, output, visited, depth+1, r);
+                visited[i] = false;
+            }
+        }
     }
 }
